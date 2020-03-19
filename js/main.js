706bd66227574e78bd4f3dd0fafd5631976510cd
Luca <mylince@gmail.com>
2020-03-19 16:25:06 +0100

    Perfezionato

--- js/main.js
+++ js/main.js
@@ -7,13 +7,22 @@ var apiBaseUrl = 'https://api.themoviedb.org/3';
 
 //Il mio link all'api da suddividere
 //https://api.themoviedb.org/3/movie/550?api_key=ccb9c8ef6b3a33f07b7be007336fd3e2
-$('#bottone').click(function(){
+$('#bottone').click(function(){  //----> CON CLICK <-----
+    myListMoovie()
+});
+
+$('#input').keypress(function(event){ //---------> CON TASTO ENTER <--------
+    if (event.keyCode == 13) { //---------> CON TASTO ENTER <--------
+        myListMoovie()
+    }
+});
+
+function myListMoovie(){
     var filmCercato = $('#input').val(); //Cerco nell'input
     $('#input').val('');//Cancello quello che c'' all'interno dell'input
     $('.container-film').text(''); //Cancello il risultato precedente
-    //console.log(filmCercato);
 
-$.ajax({
+    $.ajax({
     url: apiBaseUrl + '/search/movie',
     data:{
         api_key:'ccb9c8ef6b3a33f07b7be007336fd3e2', //la chiave api senza
@@ -24,33 +33,34 @@ $.ajax({
     success: function (tv) {
         var films = tv.results //mi ricavo l'"array"
         //console.log(films); //trovo tutti i film
-        for (var i = 0; i < films.length; i++) {
-            var film = films[i] //trovo i singoli film il ciclo);
-            var schedaFilm = { //creo la mia scheda film in cui ricavo tutto quello che mi servirà
-                titolo:film.title,
-                titoloOriginale:film.original_title,
-                lingua:film.original_language,
-                voto:film.vote_average
-            }
-            var titolo = schedaFilm.titoloOriginale; //creo variabile che mi trova solo i titoli dei film
+            for (var i = 0; i < films.length; i++) {
+                var film = films[i] //trovo i singoli film il ciclo);
+                var schedaFilm = { //creo la mia scheda film in cui ricavo tutto quello che mi servirà
+                    titolo:film.title,
+                    titoloOriginale:film.original_title,
+                    lingua:film.original_language,
+                    voto:film.vote_average
+                }
+                var titolo = schedaFilm.titoloOriginale; //creo variabile che mi trova solo i titoli dei film
+                ricercamiIFilm(filmCercato, titolo, schedaFilm);
+            /* ALTERNATIVA DENTRO ALLA FUNZIONE, SENZA CHIAMARE UNA FUNZIONE ESTERNA
             if (filmCercato.toLowerCase() == titolo.toLowerCase()) { //Se il testo che ho inserito è uguale a uno dei titoli, compare
                 var thisIsMyTemplate = template(schedaFilm); // Creo una variabile - template composta dal mio "array[i]" di elementi
                 $('.container-film').append(thisIsMyTemplate); //Appendo in '.container-list' il mio template (thisIsMyTemplate)
             }
-        }
-    },
+            */
+            }
+        },
         error: function(){
-            alert('errore')
-    }
-});
-});
+            alert('errore');
+        }
+    });
 
+};
 
-
-
-/*
-    if (filmCercato == titolo) {
+function ricercamiIFilm(filmCercato, titolo, schedaFilm) {
+    if (filmCercato.toLowerCase() == titolo.toLowerCase()) { //Se il testo che ho inserito è uguale a uno dei titoli, compare
         var thisIsMyTemplate = template(schedaFilm); // Creo una variabile - template composta dal mio "array[i]" di elementi
         $('.container-film').append(thisIsMyTemplate); //Appendo in '.container-list' il mio template (thisIsMyTemplate)
     }
-*/
+};
